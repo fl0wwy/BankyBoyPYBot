f3aa605211f1575ccf584675e8b1340cc97dcae2
fl0wwy <114193422+fl0wwy@users.noreply.github.com>
2023-01-13 04:47:30 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -4,16 +4,27 @@ import os
 import telebot
 from telebot import types
 
-account = account_generator.Account(balance=0,salary=0)
-markup = types.ForceReply(selective=False)
-
 API = os.environ['API']
 bot = telebot.TeleBot(API)
 
+accounts = []
+markup = types.ForceReply(selective=False)
+
 @bot.message_handler(commands=['start'])
 def start(message):
+    for i in accounts:
+        if i.id == message.chat.id:
+            bot.send_message(message.chat.id,'You already have an account in this bot!')
+            return ''
     bot.send_message(message.chat.id,'Hello, This is the banking bot service. please start by entering your current balance:',reply_markup=markup)
+    account = account_generator.Account(id=message.chat.id,balance=0,salary=0)
     account.date = datetime.date.today().strftime('%d/%m/%Y')
+    accounts.append(account)
+
+def check(users,m):
+    for i in users:
+        if i.id == m.chat.id:
+            return i 
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -36,7 +47,7 @@ def reply_checker(message):
 
 @bot.message_handler(commands=['balance'])
 def balance(message):
-    bot.reply_to(message,f'Your balance is {account.balance}')
+    bot.reply_to(message,f'Your balance is {check(accounts,message).balance}')
 
 @bot.message_handler(commands=['deposit'])
 def deposit_prompt(message):
@@ -44,8 +55,8 @@ def deposit_prompt(message):
 
 def balance_change(message):
     if message.text.isdecimal():
-        account.balance = float(message.text)
-        bot.reply_to(message,f'Got it! your balance is now set to {account.balance}.\nTo input your salary for your monthly documentation use the /salary command. or refer to the /help command for further information.')
+        check(accounts,message).balance = float(message.text)
+        bot.reply_to(message,f'Got it! your balance is now set to {check(accounts,message).balance}.\nTo input your salary for your monthly documentation use the /salary command. or refer to the /help command for further information.')
     else:
         bot.reply_to(message,'Please make sure the input is a decimal value! reply to the prompt to try again.')    
 
@@ -56,13 +67,13 @@ def deposit(message):
     else:
         try:
             if len(split) == 3:
-                account.deposition(amount=split[0],sender=split[1],reason=split[2])
+                check(accounts,message).deposition(amount=split[0],sender=split[1],reason=split[2])
             else:
-                account.deposition(amount=split[0],sender=split[1])     
+                check(accounts,message).deposition(amount=split[0],sender=split[1])     
         except TypeError:
             bot.reply_to(message,'Please make sure you have the right specifications!')
         else:
-            bot.reply_to(message,f'Deposit successful! {account.deposits[split[1]]}\nBalance: {account.balance}')        
+            bot.reply_to(message,f'Deposit successful! {check(accounts,message).deposits[split[1]]}\nBalance: {check(accounts,message).balance}')        
 
 @bot.message_handler(commands=['expense'])
 def expense_promot(message):
@@ -74,11 +85,11 @@ def expense(message):
         bot.reply_to(message,'Please make sure you seperate the information using a comma.')
     else:
         try:
-            account.expense(amount=split[0],place=split[1],products=[split[2::]])
+            check(accounts,message).expense(amount=split[0],place=split[1],products=[split[2::]])
         except TypeError:
             bot.reply_to(message,'Please make sure the amount is specified first!')
         else:
-            bot.reply_to(message,f'Expense documented. {account.expenses[split[1]]}\nBalance: {account.balance}')
+            bot.reply_to(message,f'Expense documented. {check(accounts,message).expenses[split[1]]}\nBalance: {check(accounts,message).balance}')
 
 @bot.message_handler(commands=['salary'])
 def salary_prompt(message):
@@ -86,33 +97,34 @@ def salary_prompt(message):
 
 def salary_change(message):
     if message.text.isdecimal():
-        account.salary = float(message.text)
-        bot.reply_to(message,f'Got it! Your monthly salary is now set to {account.salary}')
+        check(accounts,message).salary = float(message.text)
+        bot.reply_to(message,f'Got it! Your monthly salary is now set to {check(accounts,message).salary}')
     else:
         bot.reply_to(message,'Please make sure your salary input consists of only decimal values!')
 
-def briefing(message,account):
-    bot.send_message(message.chat.id,f'EXPENSES: {account.expenses}')
-    bot.send_message(message.chat.id,f'DEPOSITS: {account.deposits}')
+def briefing(message,acc):
+    bot.send_message(message.chat.id,f'EXPENSES: {acc.expenses}')
+    bot.send_message(message.chat.id,f'DEPOSITS: {acc.deposits}')
 
 @bot.message_handler(commands=['summary'])
 def summary(message):
-    last_date = account.date
+    last_date = check(accounts,message).date
     bot.send_message(message.chat.id,f'Your recent expenses and deposits from -{last_date}- to -{datetime.date.today().strftime("%d/%m/%Y")}-:')
-    briefing(message,account)
-    account.date = datetime.date.today().strftime("%d/%m/%Y")
+    briefing(message,check(accounts,message))
+    check(accounts,message).date = datetime.date.today().strftime("%d/%m/%Y")
 
 @bot.message_handler(commands=['month'])
 def income(message):
     bot.reply_to(message,f'This is your monthly summary of -{datetime.date.today().strftime("%B")}-')
-    account.deposition(account.salary,'MONTHLY INCOME')
-    bot.send_message(message.chat.id,f'YOUR BALANCE AFTER MONTHLY INCOME OF {account.salary}: {account.balance}')
-    briefing(message,account)
-    account.deposits = {}
-    account.expenses = {}
+    check(accounts,message).deposition(check(accounts,message).salary,'MONTHLY INCOME')
+    bot.send_message(message.chat.id,f'YOUR BALANCE AFTER MONTHLY INCOME OF {check(accounts,message).salary}: {check(accounts,message).balance}')
+    briefing(message,check(accounts,message))
+    check(accounts,message).deposits = {}
+    check(accounts,message).expenses = {}
 
 
 bot.polling()              
+
 
 
 
